169975d028f4ae932c7982b2074e4d6a3ad9095d
Darrius <894403325@qq.com>
2022-01-02 17:24:08 +0800

    上传Austin项目

---
+++ austin-web/src/main/java/com/darrius/austin/AustinApplication.java
@@ -0,0 +1,17 @@
+package com.darrius.austin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * @author Darrius
+ */
+@RefreshScope
+@SpringBootApplication
+public class AustinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AustinApplication.class, args);
+    }
+}
